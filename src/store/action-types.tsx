13c920a2f1e2a1d0a6046e5ab0b658585bbe0a0a
wenjuan_sun <sunwenjuan@syberos.com>
2019-11-28 18:42:24 +0800

    ddd

--- src/store/action-types.tsx
+++ src/store/action-types.tsx
@@ -6,4 +6,6 @@ export const LOGOUT = 'LOGOUT';
 
 export const CHANGE_AVATAR = 'CHANGE_AVATAR';
 
-export const GET_SLIDERS = 'GET_SLIDERS'; 
+export const GET_SLIDERS = 'GET_SLIDERS';
+export const SET_LESSONS_LOADING = 'SET_LESSONS_LOADING'; // 改变lessons中的loading状态
+export const SET_LESSONS_ = 'SET_LESSONS_';  // 把拿到的课程数据放到仓库中
--- src/store/actions/home.tsx
+++ src/store/actions/home.tsx
@@ -1,6 +1,8 @@
 import * as TYPES from "../action-types";
-import { TypeAction } from "../../typings/common";
-import { getSliders } from '../../api/home';
+import { TypeAction, TypeThunkFunction } from "../../typings/common";
+import { getSliders, getLessons } from '../../api/home';
+import { Dispatch, Store } from "redux";
+import { message } from "antd";
 export default {
   setCurrentCategory(payload: string):TypeAction {
     return {type: TYPES.SET_CURRENT_CATEGORY, payload }
@@ -9,6 +11,21 @@ export default {
     return {
       type: TYPES.GET_SLIDERS,
       payload: getSliders()
+    }
+  },
+  getLessons(): TypeThunkFunction{
+    return  async function(dispatch: Dispatch, getState:Store['getState']) {
+      let { currentCategory, lessons: { loading, limit, offset,  hasMore}} = getState().home;
+      if(!loading && hasMore) {
+        dispatch({type: TYPES.SET_LESSONS_LOADING, payload: true }) // 让lessons处于加载中
+        let result: any = await getLessons(currentCategory, offset, limit);
+        if(result.code == 0) {
+          dispatch({type: TYPES.SET_LESSONS_,payload: result.data});
+        } else {
+          message.error(result.error)
+        }
+    
+       }
     }
   }
 }
